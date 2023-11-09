5bd40a70269e6b965155a23e91a9434c7589dbf8
Gustavo Buratto <gustavo_buratto@hotmail.com>
2023-11-09 00:13:46 -0300

    Implementa back-end para guardar o horário definido
    - Cria arquivo horario.js com métodos GET e PUT e horarioSalvo.js para
      armazenar o horário definido pelo usuário
    - Adiciona dependências do back-end
    - Cria rota no back-end para '/horario'
    - Remove o arquivo evento.js e a rota '/evento'
    - Altera o arquivo contagem-regressiva.js de modo a utilizar o horário
      fornecido pelo back-end (método GET) e armazenar o horário (método
      PUT) quando ele for alterado pelo usuário

--- js/contagem-regressiva.js
+++ js/contagem-regressiva.js
@@ -6,61 +6,78 @@ const contagem = document.querySelector('#contagem');
 const proxExecucao = document.querySelector('#proxima-execucao');
 
 let horario = new Date(Date.now());
-const horDef = document.querySelector('#horario-script').value;
-const [horaDef, minutosDef] = horDef.split(':');
-horario.setTime(Date.now());
-horario.setHours(horaDef,minutosDef,0,0);
-
-// Quando o botão "Atualizar" for pressionado:
-const formulario = document.querySelector('form');
-formulario.addEventListener('submit', function(e) {
-
-  e.preventDefault();
-  e.stopPropagation();
-
-  // Lê o horário digitado pelo usuário:
-  const horarioDef = document.querySelector('#horario-script').value;
-  const [horaDef, minutosDef] = horarioDef.split(':');
+fetch('http://localhost:3000/horario', {
+  method: 'GET',
+}).then(
+  res => res.json()
+).then(obj => {
+  const horaDef = obj.hora;
+  const minutosDef = obj.minutos;
   horario.setTime(Date.now());
   horario.setHours(horaDef,minutosDef,0,0);
+  document.querySelector('#horario-script').value = `${horaDef}:${minutosDef}`;
 
-  return false;
+  // Quando o botão "Atualizar" for pressionado:
+  const formulario = document.querySelector('form');
+  formulario.addEventListener('submit', function(e) {
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    // Lê o horário digitado pelo usuário:
+    const horarioDef = document.querySelector('#horario-script').value;
+    const [horaDef, minutosDef] = horarioDef.split(':');
+    horario.setTime(Date.now());
+    horario.setHours(horaDef,minutosDef,0,0);
+
+    fetch('http://localhost:3000/horario', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:1234'
+      },
+      body: JSON.stringify({"hora": horaDef, "minutos": minutosDef})
+    }).then(
+      res => res.json()
+    );
+
+    return false;
+  });
+
+  setInterval(() => {  // Timer para atualização da view
+    const msFaltantes = horario.getTime() - Date.now();
+    const sFaltantes = msFaltantes / 1000;  // Converte para segundos
+    const sTruncado = Math.ceil(sFaltantes);  // Arredonda as casas decimais para cima, pois o contador não mostra os milisegundos
+    if (sTruncado > 0) {
+      s = sTruncado;
+    } else if (sTruncado == 0) {
+      s = 0;
+      iniciaScript();
+    } else {  // Quando atingir 0, executa o script e reinicia a contagem, acrescentando 1 dia à variável "horario"
+      s = 0;
+      horario.setTime(horario.getTime() + 24*60*60*1000);
+    }
+
+    // Conversão dos segundos para o formato dias:horas:minutos:segundos
+    const dias = Math.floor(s / (60 * 60 * 24));
+    const diasResto = s % (60 * 60 * 24);
+
+    const horas = Math.floor(diasResto / (60 * 60));
+    const horasResto = diasResto % (60 * 60);
+
+    const minutos = Math.floor(horasResto / 60);
+    const segundos = horasResto % 60;
+
+    contHoras.innerHTML = horas;
+    contMinutos.innerHTML = minutos;
+    contSegundos.innerHTML = segundos;
+
+    const dia = horario.getDate().toString().padStart(2, '0');
+    const mes = (horario.getMonth()+1).toString().padStart(2, '0');
+    const ano = horario.getFullYear();
+    const hora = horario.getHours().toString().padStart(2, '0');
+    const minuto = horario.getMinutes().toString().padStart(2, '0');
+    proxExecucao.innerHTML = `${dia}/${mes}/${ano} às ${hora}:${minuto}`;
+  }, 100);
 });
-
-setInterval(() => {  // Timer para atualização da view
-  const msFaltantes = horario.getTime() - Date.now();
-  const sFaltantes = msFaltantes / 1000;  // Converte para segundos
-  const sTruncado = Math.ceil(sFaltantes);  // Arredonda as casas decimais para cima, pois o contador não mostra os milisegundos
-  if (sTruncado > 0) {
-    s = sTruncado;
-  } else if (sTruncado == 0) {
-    s = 0;
-    iniciaScript();
-  } else {  // Quando atingir 0, executa o script e reinicia a contagem, acrescentando 1 dia à variável "horario"
-    s = 0;
-    horario.setTime(horario.getTime() + 24*60*60*1000);
-  }
-
-  // Conversão dos segundos para o formato dias:horas:minutos:segundos
-  const dias = Math.floor(s / (60 * 60 * 24));
-  const diasResto = s % (60 * 60 * 24);
-
-  const horas = Math.floor(diasResto / (60 * 60));
-  const horasResto = diasResto % (60 * 60);
-
-  const minutos = Math.floor(horasResto / 60);
-  const segundos = horasResto % 60;
-
-  contHoras.innerHTML = horas;
-  contMinutos.innerHTML = minutos;
-  contSegundos.innerHTML = segundos;
-
-  const dia = horario.getDate().toString().padStart(2, '0');
-  const mes = (horario.getMonth()+1).toString().padStart(2, '0');
-  const ano = horario.getFullYear();
-  const hora = horario.getHours().toString().padStart(2, '0');
-  const minuto = horario.getMinutes().toString().padStart(2, '0');
-  proxExecucao.innerHTML = `${dia}/${mes}/${ano} às ${hora}:${minuto}`;
-}, 100);
-
-
